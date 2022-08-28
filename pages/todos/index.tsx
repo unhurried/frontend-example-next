6664d259472f4373665df2c613b0be98bad63594
unhurried <unhurried@users.noreply.github.com>
2022-08-28 18:43:11 +0900

    Modify calling the list API to use SWR.

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,30 +1,25 @@
-import { Configuration, Todo, TodoApi } from '../../client-axios';
+import { Configuration, TodoApi } from '../../client-axios';
 import List from '../../components/List';
 import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import PageHeader from '../../components/PageHeader';
+import userSWR, { useSWRConfig } from 'swr';
 
 const Index = () => {
     const router = useRouter()
-    const { data } = useSession({ required: true })
-    const [items, setItems] = useState<Todo[] | null>(null)
+    const { data: sessionData } = useSession({ required: true })
 
-    useEffect(() => {
-        if (!data) return;
-
-        if (!items) {
-            const api = new TodoApi(
-                new Configuration({
-                    basePath: process.env.NEXT_PUBLIC_TODO_API_URL,
-                    accessToken: data.accessToken,
-                })
-            )
-                ; (async () => {
-                    setItems((await api.todoControllerGetList()).data.items)
-                })()
-        }
-    }, [data, items])
+    const fetcher = async () => {
+        const api = new TodoApi(
+            new Configuration({
+                basePath: process.env.NEXT_PUBLIC_TODO_API_URL,
+                accessToken: sessionData?.accessToken,
+            })
+        )
+        return (await api.todoControllerGetList()).data.items
+    }
+    const { data: todoItems, error } = userSWR(sessionData? 'todo_list' : null, fetcher)
+    const { mutate } = useSWRConfig()
 
     const onUpdate = (id: string) => {
         router.push(`/todos/${id}`)
@@ -33,32 +28,21 @@ const Index = () => {
         const api = new TodoApi(
             new Configuration({
                 basePath: process.env.NEXT_PUBLIC_TODO_API_URL,
-                accessToken: data!.accessToken,
+                accessToken: sessionData!.accessToken,
             })
         )
         ;(async () => {
             await api.todoControllerDelete(id)
-            fetchList();
-        })()
-    }
-    const fetchList = () => {
-        const api = new TodoApi(
-            new Configuration({
-                basePath: process.env.NEXT_PUBLIC_TODO_API_URL,
-                accessToken: data!.accessToken,
-            })
-        )
-        ;(async () => {
-            setItems((await api.todoControllerGetList()).data.items)
+            mutate('todo_list')
         })()
     }
 
-    if (!data || !items) return <>Loading ...</>
-
+    if (error) return <>Something wrong happend.</>
+    if (!todoItems) return <>Loading ...</>
     return (
         <>
             <PageHeader router={router} buttons={[{ title: 'New Item', href: '/todos/new' }]}>List</PageHeader>
-            <List items={items} onUpdate={onUpdate} onDelete={onDelete}></List>
+            <List items={todoItems!} onUpdate={onUpdate} onDelete={onDelete}></List>
         </>
     )
 }
